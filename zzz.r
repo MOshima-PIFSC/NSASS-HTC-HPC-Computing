c85273c519d30dcf1b4850d36b3dfbd6f1b2094d
N-DucharmeBarth-NOAA <nicholas.ducharme-barth@noaa.gov>
2024-10-18 13:26:11 -1000

    add renv

---
+++ zzz.r
@@ -0,0 +1,8 @@
+# libraries that are needed but aren't referenced in the code
+# needed to parse package dependencies within Quarto files
+library(yaml)
+library(rmarkdown)
+library(knitr)
+# below libraries are only needed for VSCode-R functionality
+library(rlang)
+library(jsonlite)
